7f2346fbe927aba32aa3424016bb3dd56b44f8b7
Daniel Dawson <dannyldawson@gmail.com>
2018-12-02 10:27:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
 
+def sort_array_asc(numbers)
+  numbers.sort 
+end
+
+def sort_array_desc(numbers)
+  numbers.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  temp_el = array[1]
+  array[1] = array[2]
+  array[2] = temp_el
+  array
+end
+
+def reverse_array(integers)
+  reversed_array = []
+  i = integers.size 
+  while i > 0 do 
+    i -= 1
+    reversed_array << integers[i]
+  end
+  reversed_array
+end
+
+def kesha_maker(strings)
+  new_array = []
+  strings.each do |name|
+    new_array << name.
+  end
+end
